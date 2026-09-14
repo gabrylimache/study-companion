9cbc7dc99e216a6c907cedf3f47f5fcafc7bd275
Gabry Gonza <gabriele.gonzalo2006@gmail.com>
2026-09-14 03:52:56 +0200

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => ({
-  // Set base path for GitHub Pages when building for production
-  base: mode === 'production' ? '/studycompanion/' : '/',
-  plugins: [react()],
+    // Set base path for GitHub Pages when building for production
+    base: mode === 'production' ? '/study-companion/' : '/',
+    plugins: [react()],
 }))
